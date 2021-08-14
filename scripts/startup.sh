fbbc6016ff740e0a27b3164c71dd73a24df7395e
fedx-sudo <fedx-sudo@pm.me>
2021-08-14 07:47:38 -0600

    added startup.sh script

---
+++ scripts/startup.sh
@@ -0,0 +1,7 @@
+#!/bedrock/strata/alpine/bin/sh
+
+# This is an example script of startup applications. 
+# This is configured through Plasma's settings, startup>autostart>login-scripts. 
+# The default location is ~/.local/
+
+krunner
