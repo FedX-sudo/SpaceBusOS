b5f41472f54c4dd201ac0aae4617e66f2af3be8d
fedx-sudo <fedx-sudo@pm.me>
2021-09-06 06:54:42 -0600

    updated install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,25 @@ echo "Determining system architecture."
 arch=$(uname -m)
 
 if [ "$arch" == "x86_64" ]; then
-	echo "This is an x86_64 machine."
-	echo "fetching Bedrock x86_64 to ~/.cache/."
-	wget --output-document ~/.cache/bedrock-linux.sh https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.23/bedrock-linux-0.7.23-x86_64.sh
-elif [ "$arch" = "aarch64" ]; then
-	echo "this is an aarch64 machine"
+	dl_url=$(echo "https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.23/bedrock-linux-0.7.23-x86_64.sh")
+
+elif [ "$arch" == "aarch64" ]; then
+	dl_url=$(echo "https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.23/bedrock-linux-0.7.23-aarch64.sh")
+
+elif [ "$arch" == "armv7hl" ]; then
+    dl_url=$(echo "https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.23/bedrock-linux-0.7.23-armv7hl.sh")
+
+elif [ $arch = "i386" ]; then
+    dl_url=$(echo "https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.23/bedrock-linux-0.7.23-i386.sh")
+
+elif [ $arch = "i486" ]; then
+    dl_url=$(echo "https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.23/bedrock-linux-0.7.23-i486.sh")
+
 fi
+
+echo "Downolading the Bedrock Linux installer."
+
+wget --output-file ~/.cache/bedrock-linux-0.7.23.sh $dl_url
 
 echo "download completed."
 echo "starting the Bedrock installer."
