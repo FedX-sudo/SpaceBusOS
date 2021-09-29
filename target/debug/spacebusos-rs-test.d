8cc9368b2f54018f8894145103cc39371d98c666
FedX <FedX-sudo@pm.me>
2021-09-29 12:28:21 -0600

    Delete target/debug directory

--- target/debug/spacebusos-rs-test.d
+++
@@ -1,1 +0,0 @@
-/home/fedx/Code/SpaceBusOS/target/debug/spacebusos-rs-test: /home/fedx/Code/SpaceBusOS/src/command_check.rs /home/fedx/Code/SpaceBusOS/src/config.rs /home/fedx/Code/SpaceBusOS/src/main.rs
