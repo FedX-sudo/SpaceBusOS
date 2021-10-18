1f705287063ca875e23ed3ff55b1e8a03273f82c
fedx-sudo <fedx-sudo@pm.me>
2021-10-18 09:55:04 -0600

    small changes

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+echo "This is the Alpha of the SpaceBusOS installer. Proeceed with caution."
+
+echo "Available versions:"
+echo "Laptop (1)"
+
+
+read -p "Please select which version you would like (1). " eddition
+echo "$eddition"
+if [ "$eddition" == "1" ]; then
+    echo "Proceeeding with install"
+    
+    ./laptop-install.sh
+fi
+echo "Error, no eddition specified. Abborting." 
